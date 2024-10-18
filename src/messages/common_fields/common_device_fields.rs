81de9fc845dd3442fc4884a3f3f0883a95773897
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-10-18 17:20:01 +0000

    Add serialisation, deserialisation, and test

--- src/messages/common_fields/common_device_fields.rs
+++ src/messages/common_fields/common_device_fields.rs
@@ -1,24 +1,44 @@
 //! Fields common to most messages
 
+use derive_more::derive::{Constructor, From};
+// use derive_new::new;
+use serde::{Deserialize, Serialize};
+
 /// Alias
 ///
 /// A name to present to other devices.
 /// Should be recognizable and easy to discern.
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
 pub(in super::super) struct Alias(String);
 
-/// Localsend protocol version
-pub(in super::super) struct Version {
-    major: u32,
-    minor: u32,
-}
+/// Localsend protocol version (major.minor)
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
+pub(in super::super) struct Version(String);
+
+// impl FromStr for Version {
+//     type Err = VersionDecodeError;
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         if let Some((major, minor)) = s.split_once('.') {
+//             return Ok(Version {
+//                 major: major.parse()?,
+//                 minor: minor.parse()?,
+//             });
+//         } else {
+//             Err(VersionDecodeError::NoDotFound)
+//         }
+//     }
+// }
 
 /// Device Model
 ///
 /// ex Samsung / Windows / Linux
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
 pub(in super::super) struct DeviceModel(String);
 
 /// Device type:
 ///  mobile | desktop | web | headless | server
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub(in super::super) enum DeviceType {
     Mobile,
     Desktop,
@@ -31,9 +51,12 @@ pub(in super::super) enum DeviceType {
 ///
 /// Unique string identifying the device.
 /// Only used to ignore messages from self.
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
 pub(in super::super) struct Fingerprint(String);
 
 /// Device Info
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub(in super::super) struct DeviceInfo {
     alias: Alias,
     version: Version,
@@ -43,10 +66,13 @@ pub(in super::super) struct DeviceInfo {
 }
 
 /// Port
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
 pub(in super::super) struct Port(u16);
 
 /// Protocol:
 /// http / https
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub(in super::super) enum Protocol {
     Http,
     Https,
@@ -55,15 +81,18 @@ pub(in super::super) enum Protocol {
 /// Download
 ///
 /// Prefer download API (recievers pull) over upload API (senders push)
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
 pub(in super::super) struct PreferDownload(bool);
 
-/// Announce
-///
-/// Denotes if a discovery message is of type announce or response.
-/// A discovery response is only triggered when announce is true.
-pub(in super::super) struct IsAnnounce(bool);
+// /// Announce
+// ///
+// /// Denotes if a discovery message is of type announce or response.
+// /// A discovery response is only triggered when announce is true.
+// #[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
+// pub(in super::super) struct IsAnnounce(bool);
 
 /// Session ID
 ///
 /// A shared secret that can be used to authorise upload / download,to / from server
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
 pub(in super::super) struct SessionId(String);
--- src/messages/discover.rs
+++ src/messages/discover.rs
@@ -1,4 +1,7 @@
-use super::common_fields::{DeviceInfo, IsAnnounce, Port, PreferDownload, Protocol};
+use derive_more::derive::Constructor;
+use serde::{Deserialize, Serialize};
+
+use super::common_fields::{DeviceInfo, Port, PreferDownload, Protocol};
 
 /// Multicast UDP (Default) Announcement
 ///
@@ -17,12 +20,14 @@ use super::common_fields::{DeviceInfo, IsAnnounce, Port, PreferDownload, Protoco
 ///   "announce": true
 /// }
 /// ```
-struct MulticastAnnounce {
+#[derive(Debug, Serialize, Deserialize, Constructor, PartialEq)]
+pub(crate) struct MulticastAnnounce {
+    #[serde(flatten)]
     device_info: DeviceInfo,
     port: Port,
     protocol: Protocol,
     download: Option<PreferDownload>,
-    announce: IsAnnounce,
+    announce: serde_bool::True,
 }
 
 /// MulticastAnnounce Response
@@ -64,12 +69,14 @@ struct MulticastAnnounce {
 ///   The fingerprint is only used to avoid self-discovering.
 ///
 ///   A response is only triggered when announce is true.
-struct MulticastResponse {
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct MulticastResponse {
+    #[serde(flatten)]
     device_info: DeviceInfo,
     port: Port,
     protocol: Protocol,
     download: Option<PreferDownload>,
-    announce: Option<IsAnnounce>,
+    announce: Option<serde_bool::False>,
 }
 
 /// 3.2 HTTP (Legacy Mode)
@@ -94,7 +101,9 @@ struct MulticastResponse {
 ///   "download": true, // if the download API (5.2 and 5.3) is active (optional, default: false)
 /// }
 /// ```
-struct LegacyRegister {
+#[derive(Serialize, Deserialize)]
+pub(crate) struct LegacyRegister {
+    #[serde(flatten)]
     device_info: DeviceInfo,
     port: Port,
     protocol: Protocol,
@@ -113,8 +122,56 @@ struct LegacyRegister {
 ///   "download": true, // if the download API (5.2 and 5.3) is active (optional, default: false)
 /// }
 /// ```
-
-struct LegacyRegisterResponse {
+#[derive(Serialize, Deserialize)]
+pub(crate) struct LegacyRegisterResponse {
+    #[serde(flatten)]
     device_info: DeviceInfo,
     download: Option<PreferDownload>,
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::messages::common_fields::{Alias, DeviceModel, DeviceType, Fingerprint, Version};
+
+    use super::*;
+
+    #[test]
+    fn multicast_announce_deserialize_serialize() {
+        let multicast_announce_json = json!({
+          "alias": "Nice Orange",
+          "version": "2.0", // protocol version (major.minor)
+          "deviceModel": "Samsung", // nullable
+          "deviceType": "mobile", // mobile | desktop | web | headless | server, nullable
+          "fingerprint": "random string",
+          "port": 53317,
+          "protocol": "https", // http | https
+          "download": true, // if the download API (5.2 and 5.3) is active (optional, default: false)
+          "announce": true
+        });
+
+        let device_info = DeviceInfo::new(
+            Alias::new("Nice Orange".into()),
+            Version::new("2.0".into()),
+            Some(DeviceModel::new("Samsung".into())),
+            DeviceType::Mobile,
+            Fingerprint::new("random string".into()),
+        );
+        let constructed_multicast_announce = MulticastAnnounce::new(
+            device_info,
+            Port::new(53317),
+            Protocol::Https,
+            Some(PreferDownload::new(true)),
+            serde_bool::True,
+        );
+        // Deserialize
+        let read_multicast_announce: MulticastAnnounce =
+            serde_json::from_value(multicast_announce_json.clone()).unwrap();
+        assert_eq!(constructed_multicast_announce, read_multicast_announce);
+        // Serialize
+        let written_multicast_announce_json =
+            serde_json::to_value(constructed_multicast_announce).unwrap();
+        assert_eq!(multicast_announce_json, written_multicast_announce_json);
+    }
 }
